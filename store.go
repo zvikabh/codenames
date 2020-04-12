01520807f5c0147d9136fd650e88c4edb42e1111
Jackson Owens <jackson@cockroachlabs.com>
2020-04-12 14:50:07 -0400

    wip

--- store.go
+++ store.go
@@ -1,12 +1,15 @@
 package codenames
 
 import (
+	"bytes"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"math"
 	"time"
 
 	"github.com/cockroachdb/pebble"
+	"github.com/kr/pretty"
 )
 
 // PebbleStore wraps a *pebble.DB with an implementation of the
@@ -62,6 +65,67 @@ func (ps *PebbleStore) Save(g *Game) error {
 	return err
 }
 
+func (ps *PebbleStore) StatAdd(stat string, v uint64) error {
+	var b [binary.MaxVarintLen64]byte
+	n := binary.PutUvarint(b[:], v)
+
+	return ps.DB.Merge(
+		[]byte(fmt.Sprintf("/stats/%s", stat)),
+		b[:n],
+		nil)
+}
+
+func (ps *PebbleStore) GetStat(stat string) (uint64, error) {
+	rawV, c, err := ps.DB.Get([]byte(fmt.Sprintf("/stats/%s", stat)))
+	if err != nil {
+		return 0, fmt.Errorf("db.Get: %w", err)
+	}
+	defer c.Close()
+	v, n := binary.Uvarint(rawV)
+	if n < 0 {
+		return 0, fmt.Errorf("unable to read stat value: %v", rawV)
+	}
+	return v, nil
+}
+
+// PebbleMerge implements the pebble.Merge function type.
+func PebbleMerge(k, v []byte) (pebble.ValueMerger, error) {
+	if bytes.HasPrefix(k, []byte("/stats/")) {
+		return &addValueMerger{}, nil
+	}
+	return nil, fmt.Errorf("unrecognized merge key: %s", pretty.Sprint(k))
+}
+
+// addValueMerger implements pebble.ValueMerger by interpreting values as a
+// unsigned varint and adding its operands.
+type addValueMerger struct {
+	v uint64
+}
+
+func (m *addValueMerger) MergeNewer(value []byte) error {
+	v, n := binary.Uvarint(value)
+	if n < 0 {
+		return fmt.Errorf("unable to read merge value: %v", value)
+	}
+	m.v = m.v + v
+	return nil
+}
+
+func (m *addValueMerger) MergeOlder(value []byte) error {
+	v, n := binary.Uvarint(value)
+	if n < 0 {
+		return fmt.Errorf("unable to read merge value: %v", value)
+	}
+	m.v = m.v + v
+	return nil
+}
+
+func (m *addValueMerger) Finish() ([]byte, error) {
+	b := make([]byte, binary.MaxVarintLen64)
+	n := binary.PutUvarint(b, m.v)
+	return b[:n], nil
+}
+
 func gameKV(g *Game) (key, value []byte, err error) {
 	value, err = json.Marshal(g)
 	if err != nil {
@@ -80,4 +144,6 @@ func mkkey(unixSecs int64, id string) []byte {
 
 type discardStore struct{}
 
-func (ds discardStore) Save(*Game) error { return nil }
+func (_ discardStore) Save(*Game) error               { return nil }
+func (_ discardStore) GetStat(string) (uint64, error) { return 0, nil }
+func (_ discardStore) StatAdd(string, uint64) error   { return nil }
